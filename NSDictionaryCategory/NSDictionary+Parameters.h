723ab6352390348d94830cce4dedd767376957dc
JakeBaek <jake.baek@gmail.com>
2011-07-15 13:32:55 +0900

    added - NSArray+Strings - NSDictionary+Parameters - NSObject+PerformSelector - NSString+Entities - NSString+RemoveSuffix - NSString+Tags - NSString+URLEncoding - NSURL+Parameters - UIActionSheet+Context - UIApplication+ActivityIndicator

---
+++ NSDictionaryCategory/NSDictionary+Parameters.h
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+Parameters.h
+//  newsyc
+//
+//  Created by Grant Paul on 3/13/11.
+//  Copyright 2011 Xuzz Productions, LLC. All rights reserved.
+//
+
+@interface NSDictionary (Parameters)
+
+- (NSString *)queryString;
+
+@end
